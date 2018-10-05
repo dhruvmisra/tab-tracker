c9738d9aba3ce7678c646945e1c274d66ec84c64
dhruvmisra <40134655+dhruvmisra@users.noreply.github.com>
2018-10-05 18:00:39 +0530

    setup of express server and vue

---
+++ client/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h1>Register</h1>
+      <input type="email" name="email" placeholder="Email" v-model="email">
+      <br>
+      <input type="password" name="password" placeholder="Password" v-model="password">
+      <br>
+      <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        register() {
+            console.log('Register was clicked', this.email, this.password);
+        }
+    }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
